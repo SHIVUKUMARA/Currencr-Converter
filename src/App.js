0062c78d96b98472c45259f8c7ac9cb16282039a
SHIVUKUMARA <shivukumaraspatil01@gmail.com>
2024-02-08 22:42:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Currency from "./CurrencyConverter/Currency";
+function App() {
+  return (
+    <div>
+      <Currency />
+    </div>
+  );
+}
+
+export default App;
+  
